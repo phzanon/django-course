224b6b77cfe1322fae4b9bb25ba4856f0ccd34e1
phzanon <p.hzanon123@gmail.com>
2022-09-24 13:58:43 -0300

    first commit

---
+++ visitantes/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from visitantes.models import Visitante
+
+class VisitanteForm(forms.ModelForm):
+    class Meta:
+        model = Visitante
+        fields = [
+            "nome_completo", "cpf", "data_nascimento", "numero_casa", "placa_veiculo"
+        ]
+        error_messages = {
+            "nome_completo": {
+                "required": "O nome completo do visitante é obrigatório para o registro"
+            },
+            "cpf": {
+                "required": "O CPF do visitante é obrigatório para o registro"
+            },
+            "data_nascimento": {
+                "required": "A data de nascimento do visitante é obrigatório para o registro",
+                "invalid": "Por favor informe um formato válido para a data de nascimento (DD/MM/AAAA)"
+            },
+            "numero_casa": {
+                "required": "Por favor, informe o número da casa a ser visitada"
+            },
+        }
